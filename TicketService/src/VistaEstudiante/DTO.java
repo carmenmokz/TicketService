118dddbbd2695915675cb6fb1ad36d1228ae74ed
Unknown <luismorah77@gmail.com>
2018-04-15 23:01:58 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ TicketService/src/VistaEstudiante/DTO.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VistaEstudiante;
+
+/**
+ *
+ * @author Luis Mora
+ */
+public class DTO {
+
+    public DTO(String tipoTiquete, String nombreProperties, String nombrePDF, String nombreZIP) {
+        this.tipoTiquete = tipoTiquete;
+        this.nombreProperties = nombreProperties;
+        this.nombrePDF = nombrePDF;
+        this.nombreZIP = nombreZIP;
+    }
+    public DTO(String tipoTiquete, String nombreProperties, String nombrePDF) {
+        this.tipoTiquete = tipoTiquete;
+        this.nombreProperties = nombreProperties;
+        this.nombrePDF = nombrePDF;
+    }
+    public DTO(String tipoTiquete, String nombreProperties,String nombreZIP,int dummy) {
+        this.tipoTiquete = tipoTiquete;
+        this.nombreProperties = nombreProperties;
+        this.nombreZIP = nombreZIP;
+    }
+
+    public String getTipoTiquete() {
+        return tipoTiquete;
+    }
+
+    public void setTipoTiquete(String tipoTiquete) {
+        this.tipoTiquete = tipoTiquete;
+    }
+
+    public String getNombreProperties() {
+        return nombreProperties;
+    }
+
+    public void setNombreProperties(String nombreProperties) {
+        this.nombreProperties = nombreProperties;
+    }
+
+    public String getNombrePDF() {
+        return nombrePDF;
+    }
+
+    public void setNombrePDF(String nombrePDF) {
+        this.nombrePDF = nombrePDF;
+    }
+
+    public String getNombreZIP() {
+        return nombreZIP;
+    }
+
+    public void setNombreZIP(String nombreZIP) {
+        this.nombreZIP = nombreZIP;
+    }
+    private String tipoTiquete;
+    private String nombreProperties;
+    private String nombrePDF;
+    private String nombreZIP;
+    
+    
+}
